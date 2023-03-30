e0761802a151bd56413be4d6e0c27aaa7cdbf3fa
이주원 <juwoon7163@gmail.com>
2023-03-30 19:37:18 +0900

    .

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ app.add_middleware(
 
 @app.on_event("startup")
 async def startup():
-    selfstudy_thread = BackgroundApply(apply_type="selfstudy", apply_time=(20, 00))  # 8시 정각에 신청
-    massage_thread = BackgroundApply(apply_type="massage", apply_time=(20, 20))  # 8시 20분에 신청
+    selfstudy_thread = BackgroundApply(apply_type="selfstudy", apply_time=(11, 00))  # 8시 정각에 신청
+    massage_thread = BackgroundApply(apply_type="massage", apply_time=(11, 20))  # 8시 20분에 신청
     selfstudy_thread.start()
     massage_thread.start()
